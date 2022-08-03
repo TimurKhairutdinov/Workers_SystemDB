3cfe31d9dd83d812d4751a1d08cf9f6c8ff0dabe
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-08-03 23:04:52 +0300

    Create menu, add functions - contact: add, find, edit, del. Files: save, load (json, csv)

--- control.py
+++ control.py
@@ -1,39 +1,19 @@
 import json
 
 
-def add_worker():
-    global name, comment, number_tel, type_doc, type_worker, id_number,department
-    id_number = input('Введите номер сотрудника >> ')
-    name = input('Введите Имя Фамилию >> ')
-    type_doc = input('Введите тип документа >> ')
-    number_tel = input('Введите номер телефона >> ')
-    department = input('Введите одтел >> ')
-    type_worker = input('Введите доджность >> ')
-    comment = input('Введите комментарий >> ')
+def save_d(arg):
+    with open('table_workers.json', 'w', encoding='utf-8') as file:
+        file.write(json.dumps(arg, ensure_ascii=False))
+
+def save2(arg):
+    with open('table_workers.csv', 'a', encoding='utf-8') as file:
+        file.write(json.dumps(arg, ensure_ascii=False))
+
+def load_d():
+    with open('table_workers.json', 'r+', encoding='utf-8') as file:
+        table = json.load(file)
+        print(type(table))
+
+load_d()
 
 
-def init_data():
-    add_worker()
-    global data_worker
-    data = [id_number,name,type_doc,number_tel,department,type_worker,comment]
-    data_worker = {}
-    data_worker[name] = data
-    return data_worker
-
-
-def save_data_workers():
-    
-    with open('workers.json', 'a', encoding = 'utf-8') as workers:
-        workers.write(json.dumps(init_data(), ensure_ascii=False))
-        print('Save')
-
-
-def save_data_json():
-    with open('book.json', 'a', encoding='utf-8') as telephon_book:
-        telephon_book.write(json.dumps(init_data(), ensure_ascii=False))
-
-
-def load_data_json():
-    with open('workers.json', 'r', encoding='utf-8') as telephon_book:
-        data_from_telephon_book = json.load(telephon_book)
-        return data_from_telephon_book
--- menu.py
+++ menu.py
@@ -1,16 +1,83 @@
-
+from curses.ascii import isdigit
+import id_number as id
+import json
 def menu_admin():
     print('Введите пароль: ')
 
 
 def menu_user():
     print('База данных')
-    a = input(
+    choice = input(
         'Для работы, введите номер пункта меню.\n'
         '1. Добавить. \n'
         '2. Поиск. \n'
-        '3. Изменить. \n')
-    match a:
-        case 1: а
-        case 2: а
-        case 3: а
+        '3. Изменить. \n'
+        '>> ')
+    
+    match choice:
+        case 1: add_contact()
+        case 2: find_contact()
+        case 3: del_contact()
+
+
+def add_contact():
+    global id_number,name,type_doc, number_tel, department, type_worker, comment, all_workers
+    
+    id_number = id.set_id()
+    name = input('Введите Имя Фамилию >> ')
+    type_doc = input('Введите тип документа >> ')
+    number_tel = input('Введите номер телефона >> ')
+    department = input('Введите одтел >> ')
+    type_worker = input('Введите доджность >> ')
+    comment = input('Введите комментарий >> ')
+
+    worker = [{'id_number' : id_number,'type_doc':type_doc,
+               'number_tel':number_tel,'department':department,
+               'type_worker':type_worker,'comment':comment}]
+    
+    all_workers = load_json()
+    all_workers[name] = worker
+    save_json(all_workers)
+
+
+def del_contact():
+    print('Введите Имя Фамилию, если вы хотите удалить по id, введите "id"')
+    name = input('>> ')
+    all_workers = load_json()
+    all_workers.pop(name)
+    save_json(all_workers)
+    
+    
+def edit_contact():
+    return 0
+
+def find_contact():
+    finder = init_find()
+    all_workers = load_json()
+    if finder == str:
+        all_workers.pop(name)
+    else:
+        lst = all_workers.get(name)
+        
+
+def save_json(arg):
+    with open('table_workers.json', 'w', encoding='utf-8') as file:
+        file.write(json.dumps(arg, ensure_ascii=False))
+
+def save_csv(arg):
+    with open('table_workers.csv', 'a', encoding='utf-8') as file:
+        file.write(json.dumps(arg, ensure_ascii=False))
+
+def load_json():
+    with open('table_workers.json', 'r+', encoding='utf-8') as file:
+        table = json.load(file)
+        return table
+    
+def init_find():
+    print('Введите Имя Фамилию или "id"')
+    msg = str(input('>> '))
+    if msg.isdigit():
+        return int(msg)
+    else:
+        return msg
+    
