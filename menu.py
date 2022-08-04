bc34c4bd1ee6a41d580788f23cbb182cac0c6024
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-08-05 00:14:52 +0300

    Create 'Menu':menu_user, menu_admin. Functions menu: add_contact, del_contact, edit_contact, find_contact, view_workers, init_find

--- menu.py
+++ menu.py
@@ -1,6 +1,8 @@
-from curses.ascii import isdigit
-import id_number as id
 import json
+import id_number as id
+import files_editor as fe
+
+
 def menu_admin():
     print('Введите пароль: ')
 
@@ -12,72 +14,82 @@ def menu_user():
         '1. Добавить. \n'
         '2. Поиск. \n'
         '3. Изменить. \n'
+        '4. Удалить.\n'
+        '5. Показать список работников\n'
         '>> ')
     
     match choice:
-        case 1: add_contact()
-        case 2: find_contact()
-        case 3: del_contact()
-
+        case '1': add_contact()
+        case '2': print(find_contact())
+        case '3': edit_contact()
+        case '4': del_contact()
+        case '5': view_workers()
 
 def add_contact():
-    global id_number,name,type_doc, number_tel, department, type_worker, comment, all_workers
+    global id_number, name, type_doc, number_tel, department, type_worker, comment, all_workers
     
-    id_number = id.set_id()
     name = input('Введите Имя Фамилию >> ')
     type_doc = input('Введите тип документа >> ')
     number_tel = input('Введите номер телефона >> ')
-    department = input('Введите одтел >> ')
-    type_worker = input('Введите доджность >> ')
+    department = input('Введите отдел >> ')
+    type_worker = input('Введите должность >> ')
     comment = input('Введите комментарий >> ')
+    id_number = id.set_id()
 
-    worker = [{'id_number' : id_number,'type_doc':type_doc,
-               'number_tel':number_tel,'department':department,
-               'type_worker':type_worker,'comment':comment}]
+    worker = [{'id_number': id_number, 'type_doc': type_doc,
+               'number_tel': number_tel, 'department': department,
+               'type_worker': type_worker, 'comment': comment}]
     
-    all_workers = load_json()
+    id_to_name = fe.load_json('data_files\id_to_name.json')
+    id_to_name[id_number] = name
+    fe.save_json(id_to_name,'data_files\id_to_name.json')
+    all_workers = fe.load_json()
     all_workers[name] = worker
-    save_json(all_workers)
+    fe.save_json(all_workers)
 
 
 def del_contact():
     print('Введите Имя Фамилию, если вы хотите удалить по id, введите "id"')
     name = input('>> ')
-    all_workers = load_json()
+    all_workers = fe.load_json()
     all_workers.pop(name)
-    save_json(all_workers)
-    
-    
+    fe.save_json(all_workers)
+
+
 def edit_contact():
     return 0
 
+
 def find_contact():
     finder = init_find()
-    all_workers = load_json()
-    if finder == str:
-        all_workers.pop(name)
+    if finder.isdigit():
+        id_to_name = fe.load_json('data_files\id_to_name.json')
+        try:
+            name = id_to_name.get(finder)
+            all_workers = fe.load_json()
+            return f'{name} : {all_workers.get(name)}'
+        except:
+            print('Пользователь не найден')
+            find_contact()
+
     else:
-        lst = all_workers.get(name)
+        all_workers = fe.load_json()
+        try:
+            return f'{finder} : {all_workers.get(finder)}'
+        except:
+            print('Пользователь не найден')
+            find_contact()
         
-
-def save_json(arg):
-    with open('table_workers.json', 'w', encoding='utf-8') as file:
-        file.write(json.dumps(arg, ensure_ascii=False))
-
-def save_csv(arg):
-    with open('table_workers.csv', 'a', encoding='utf-8') as file:
-        file.write(json.dumps(arg, ensure_ascii=False))
-
-def load_json():
-    with open('table_workers.json', 'r+', encoding='utf-8') as file:
-        table = json.load(file)
-        return table
-    
+        
+        
 def init_find():
     print('Введите Имя Фамилию или "id"')
-    msg = str(input('>> '))
-    if msg.isdigit():
-        return int(msg)
-    else:
-        return msg
+    msg = input('>> ')
+    return msg
+    
+def view_workers():
+    dict = fe.load_json('data_files\id_to_name.json')
+    for i in dict.keys():
+        print(f'{i}: {dict.get(i)}')
+    
     
