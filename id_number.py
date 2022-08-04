74f9b1b40e95e6e1ea4ae4617237dd1140191532
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-08-05 00:12:24 +0300

    Create functions: get_id, set_id, save_id

--- id_number.py
+++ id_number.py
@@ -1,9 +1,11 @@
 import json
 
+import files_editor as fe
+
 
 def get_id():
     global id_numbers
-    id_numbers = load_id_json()
+    id_numbers = fe.load_json('data_files\id_workers.json')
     for i in range(1, 10000):
         if id_numbers.count(i) == 0:
             return i
@@ -12,25 +14,10 @@ def get_id():
 def set_id():
     global id_worker
     id_worker = get_id()
+    save_id(id_worker)
     return id_worker
 
 
 def save_id(id):
     id_numbers.append(id)
-    return id_numbers
-
-
-def load_id_json():
-    with open('id_workers.json', 'r', encoding='utf-8') as id_list:
-        id = json.load(id_list)
-        return id
-
-
-def save_data_json():
-    with open('id_workers.json', 'w', encoding='utf-8') as id_list:
-        id_list.write(json.dumps(save_id(id_worker), ensure_ascii=False))
-        
-
-
-set_id()
-save_data_json()
+    fe.save_json(id_numbers, 'data_files\id_workers.json')
