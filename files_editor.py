caa66093b2b20ed9aaac3b7983edc8fbb84a82a3
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-08-11 22:11:53 +0300

    Update logging, formate msg text

--- files_editor.py
+++ files_editor.py
@@ -1,9 +1,18 @@
 import json
+import logging as log
+import datetime
 
+now = datetime.datetime.now()
+
+log.basicConfig(level=log.DEBUG,
+                filename="WORKERS_LOG.log",
+                encoding='utf-8')
 
 def save_json(arg, file_json=r"data_files\table_workers.json"):
     with open(f'{file_json}', 'w', encoding='utf-8') as file:
         file.write(json.dumps(arg, ensure_ascii=False))
+        log.debug(f'{now}: Save to {file_json} complete!')
+        
 
 
 def save_csv(arg, file_csv=r'data_files\table_workers.json'):
@@ -13,5 +22,6 @@ def save_csv(arg, file_csv=r'data_files\table_workers.json'):
 
 def load_json(file_json=r'data_files\table_workers.json'):
     with open(f'{file_json}', 'r', encoding='utf-8') as file:
-        table = json.load(file)
-        return table
+        item = json.load(file)
+        log.debug(f'{now}: Load from {file_json} complete!')
+        return item
